9f2c195d3287483e298072faa715d96a5b681c8f
Martin Nicholson <recyclebing+github@gmail.com>
2018-05-22 17:39:43 +0100

    Tweak to data format

--- index.js
+++ index.js
@@ -11,14 +11,18 @@ fs.readFile('sample.ged', 'utf-8', (err, data) => {
 function processTree(json) {
 	for (var node of json) {
 		if (node.tag == 'INDI') {
-			console.log(node.tag, ' ', node.pointer);
+			//console.log(node.tag, ' ', node.pointer);
 			if (node.data) console.log(node.data);
-			console.log(mapIndividual(node.tree));
+			var indiv = mapIndividual(node.tree);
+			indiv.id = node.pointer;
+			console.log(indiv);
 		}
 		else if (node.tag == 'FAM') {
-			console.log(node.tag, ' ', node.pointer);
+			//console.log(node.tag, ' ', node.pointer);
 			if (node.data) console.log(node.data);
-			console.log(mapFamily(node.tree));
+			var family = mapFamily(node.tree);
+			family.id = node.pointer;
+			console.log(family);
 		}
 	}
 }
@@ -30,16 +34,17 @@ function mapIndividual(arr) {
 		fname: arr.filter(node => node.tag == 'NAME')[0].data,
 		lname: arr.filter(node => node.tag == 'NAME')[0].data,
 		sex: arr.filter(node => node.tag == 'SEX')[0].data,
-		fams: fams.length > 0 ? fams[0].data : null,
-		famc: famc.length > 0 ? famc[0].data : null
+		famsHeadOf: fams.length > 0 ? fams.map(obj => obj.data) : null,
+		famsChildOf: famc.length > 0 ? famc[0].data : null
 	};
 }
 
 function mapFamily(arr) {
+	var children = arr.filter(node => node.tag == 'CHIL');
 	return {
 		husband: arr.filter(node => node.tag == 'HUSB')[0].data,
 		wife: arr.filter(node => node.tag == 'WIFE')[0].data,
 		married: arr.filter(node => node.tag == 'MARR')[0].data,
-		children: arr.filter(node => node.tag == 'CHIL').map(obj => obj.data)
+		children: children.length > 0 ? children.map(obj => obj.data) : null
 	};
 }
