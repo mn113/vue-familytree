23853828f9254052f5f2c307d1fb9c0a1c4f8e0e
Martin Nicholson <recyclebing+github@gmail.com>
2018-06-05 01:31:40 +0100

    Event listener attached

--- static/js/layout-cyto.js
+++ static/js/layout-cyto.js
@@ -6,17 +6,15 @@ var cy = cytoscape({
 		{
 			selector: 'node',
 			style: {
-				'label': 'data(label)'
+				'label': 'data(label)',
+				'background-color': 'lightblue'
 			}
 		},
-
 		{
 			selector: 'edge',
 			style: {
 				'width': 3,
-				'line-color': '#ccc',
-				'target-arrow-color': '#ccc',
-				'target-arrow-shape': 'triangle'
+				'line-color': '#ccc'
 			}
 		}
 	]
@@ -71,6 +69,24 @@ function addEdges() {
 function layoutAndRender() {
 	cy.layout({
 		name: 'dagre',
+		nodeSep: undefined, // the separation between adjacent nodes in the same rank
+	    edgeSep: undefined, // the separation between adjacent edges in the same rank
+	    rankSep: undefined, // the separation between adjacent nodes in the same rank
+	    rankDir: 'TB', // 'TB' for top to bottom flow, 'LR' for left to right,
+	    ranker: undefined, // Type of algorithm to assign a rank to each node in the input graph. Possible values: 'network-simplex', 'tight-tree' or 'longest-path'
+	    minLen: function( edge ){ return 1; }, // number of ranks to keep between the source and target of the edge
+	    edgeWeight: function( edge ){ return 1; }, // higher weight edges are generally made shorter and straighter than lower weight edges
+
+	    // general layout options
+	    fit: true, // whether to fit to viewport
+	    padding: 30, // fit padding
+	    spacingFactor: undefined, // Applies a multiplicative factor (>0) to expand or compress the overall area that the nodes take up
+	    nodeDimensionsIncludeLabels: true, // whether labels should be included in determining the space used by a node
+	    animate: false, // whether to transition the node positions
+	    boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
+	    transform: function( node, pos ){ return pos; }, // a function that applies a transform to the final node position
+	    ready: function(){}, // on layoutready
+	    stop: function(){} // on layoutstop
 	}).run();
 	console.log("Layout applied.");
 }
---
+++ static/js/layout-dagre.js
@@ -0,0 +1,93 @@
+// Create a new directed graph
+var g = new dagreD3.graphlib.Graph();
+
+// Set an object for the graph label
+g.setGraph({});
+
+// Default to assigning a new object as a label for each new edge.
+//g.setDefaultEdgeLabel(function() { return {}; });
+
+// Set up an SVG group so that we can translate the final graph.
+var svg = d3.select("svg"),
+	inner = svg.append("g");
+
+function addNodes() {
+	// Add nodes to the graph.
+	for (var node of app.individuals) {
+		g.setNode(node.id, {
+			labelType: "html",
+			label: `<div id="${node.id}">
+						<p>${node.fname} <b>${node.lname}</b></p>
+						<p>${year(node.start)} - ${year(node.end)}</p>
+					</div>`,
+			width: 50 + 3 * (node.fname.length + node.lname.length),
+			height: 40,
+			style: `fill: ${node.sex == "M" ? "lightblue" : node.sex == "F" ? "lightpink" : "#ccc"}`
+		});
+	}
+	for (var node of app.families) {
+		g.setNode(node.id, {
+			labelType: "html",
+			label: `<div id="${node.id}">
+						<p>${node.married}</p>
+						<p>${node.children.length} children</p>
+					</div>`,
+			shape: "ellipse",
+			width: 80,
+			height: 50,
+			style: "fill: lightyellow; stroke: #333"
+		});
+	}
+}
+
+function addEdges() {
+	for (var link of app.links) {
+		g.setEdge(link.source, link.target, { curve: d3.curveBasis });
+	}
+}
+
+function layoutAndRender() {
+	// Calculate layout:
+	dagre.layout(g);
+
+	// Create the renderer
+	var render = new dagreD3.render();
+
+	// Run the renderer. This is what draws the final graph.
+	render(inner, g);
+
+	// Add event listeners to nodes:
+	inner.selectAll("g.node").on("click", function(id) {
+		selectNode(id);
+	});
+
+	// Center the graph
+	var xCenterOffset = (svg.attr("width") - g.graph().width) / 2;
+	inner.attr("transform", "translate(" + xCenterOffset + ", 20)");
+	svg.attr("height", g.graph().height + 40);
+}
+
+function selectNode(id) {
+	console.log("Node", id, "selected");
+}
+
+function year(date) {
+	if (date === undefined || date.length === 0) return "?";
+	if (typeof date == 'number') date = new Date(date);
+	return date.getFullYear();
+}
+
+function fulldate(date) {
+	if (date === undefined || date.length === 0) return "?";
+	if (typeof date == 'number') date = new Date(date);
+	var y = date.getFullYear(),
+		m = (date.getMonth() + 1).toString().padStart(2,"0"),
+		d = date.getDate().toString().padStart(2,"0");
+	return `${y}-${m}-${d}`;
+}
+
+svg.selectAll("g.node").on("click", function(id) {
+	console.log(id);
+	var _node = g.node(id);
+	console.log("Clicked " + id, _node);
+});
