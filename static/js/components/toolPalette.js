0484eeae9b7c5dc4048c8d53a867bc24587fed51
Martin Nicholson <recyclebing+github@gmail.com>
2018-06-13 01:40:13 +0100

    Tooltips for palette

--- static/js/components/toolPalette.js
+++ static/js/components/toolPalette.js
@@ -8,8 +8,8 @@ Vue.component('tool-palette', {
     },
     template: `
     <div id="palette">
-        <vs-button vs-type="dark-filled" vs-icon="person_add" @click="$emit('add-indi')"></vs-button>
-        <vs-button vs-type="dark-filled" vs-icon="group_add" @click="$emit('add-fam')"></vs-button>
+        <vs-button vs-type="dark-filled" v-tooltip="'New Person'" vs-icon="person_add" @click="$emit('add-indi')"></vs-button>
+        <vs-button vs-type="dark-filled" v-tooltip="'New Family'" vs-icon="group_add" @click="$emit('add-fam')"></vs-button>
         <vs-button vs-type="dark-filled" vs-icon="zoom_in"></vs-button>
         <vs-button vs-type="dark-filled" vs-icon="zoom_out"></vs-button>
         <vs-button vs-type="dark-filled" vs-icon="content_cut"></vs-button>
